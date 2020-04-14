e06e547a137231639e6cf510c13aec959f8844d8
vit <9455469@mail.ru>
2020-04-14 04:21:48 +0300

    58 скрин svg + canvas

--- getScreenshot.js
+++ getScreenshot.js
@@ -1,32 +1,54 @@
 
 
+var arrImg_1 = null;
+var arrImg_2 = null;
+
 
 
 // screenshot
-function saveAsImage() 
+function createImageScene() 
 { 
-createImage();
-return;
 
 	try 
 	{	
+		showHidePoint({visible: false});
 		var background = scene.background.clone();
 		scene.background = new THREE.Color( 0xffffff );
 		infProject.scene.grid.visible = false;
 		infProject.settings.shader.fxaaPass.enabled = true;
-		//renderer.antialias = true;
 		renderer.render( scene, camera );
 		
 		var strMime = "image/png";
 		var imgData = renderer.domElement.toDataURL(strMime);	
 
-		//renderer.antialias = false;
+		showHidePoint({visible: true});
 		scene.background = background;
 		infProject.scene.grid.visible = true;
 		infProject.settings.shader.fxaaPass.enabled = false;
 		renderer.render( scene, camera );
  
-		openFileImage(imgData.replace(strMime, "image/octet-stream"), "screenshot.png");
+				
+		if(camera == camera3D)
+		{
+			openFileImage(imgData.replace(strMime, "image/octet-stream"), "screenshot.png");
+		}
+		else
+		{	
+			var svg = document.querySelector('#svgFrame');
+			var canvas = document.createElement("canvas");
+			canvas.width = svg.clientWidth;
+			canvas.height = svg.clientHeight;
+			var ctx = canvas.getContext("2d");
+			var DOMURL = self.URL || self.webkitURL || self;
+			var img = new Image(); 
+
+			img.onload = function() 
+			{
+				arrImg_1 = img;
+				assemblyImage();
+			};
+			img.src = imgData;
+		}		
 	} 
 	catch (e) 
 	{
@@ -35,6 +57,19 @@ return;
 	}
 }
 
+
+
+function showHidePoint(cdm)
+{
+	if(camera == camera3D) return;
+	
+	var point = infProject.scene.array.point;
+	
+	for( var i = 0; i < point.length; i++ )
+	{
+		point.visible = cdm.visible;
+	}
+}
 
 
 // screenshot сохраняем в bd
@@ -113,9 +148,11 @@ function convertImgToBase64(src, callback)
 
 
 
-function createImage() 
+function createImageSvg() 
 {
-
+	if(camera == camera3D) return;
+	
+	var arr = [];
 	var svg = document.querySelector('#svgFrame');
 
 	var wall = infProject.scene.array.wall;
@@ -128,21 +165,16 @@ function createImage()
 			{
 				var label = wall[i].userData.wall.html.label[i2]
 				//wall[i].userData.wall.html.label[i2].remove();
-				var txt  = document.createElementNS(infProject.settings.svg.tag, "text");
-				
-				
+				var txt  = document.createElementNS(infProject.settings.svg.tag, "text");								
 				
 				var x = label.userData.elem.x;
-				var y = label.userData.elem.y;
-				
+				var y = label.userData.elem.y;				
 				
 				var translate = 'translate('+ ((label.clientWidth/2)*-1) +',0)';
 				var rotate = 'rotate('+THREE.Math.radToDeg(label.userData.elem.rot)+','+(x+(label.clientWidth/2)*-1)+','+(y+label.clientHeight/2)+')';
 				
 				var transform = translate;
 				var str = label.innerText;
-				
-				
 				
 				txt.setAttribute('x', x);
 				txt.setAttribute('y', y);
@@ -152,19 +184,15 @@ function createImage()
 				txt.textContent = str;
 
 				svg.appendChild(txt);
+				
+				arr[arr.length] = txt;
 			}
 		}					
 	}
 
-	console.log(wall[0].userData.wall.html.label[0]);
-	console.log(wall[0].userData.wall.html.label[0].style.transform);
-	console.log(wall[0].userData.wall.html.label[0].style.top);
-	console.log(wall[0].userData.wall.html.label[0].innerText);
-	console.log(wall[0].userData.wall.html.label[0].offsetLeft);
-	console.log(wall[0].userData.wall.html.label[0].offsetTop);
 
 
-	var svg = document.querySelector('#svgFrame');
+	
 	svg.setAttribute('width', svg.clientWidth);
 	svg.setAttribute('height', svg.clientHeight);
 	var svgString = new XMLSerializer().serializeToString(svg);
@@ -181,19 +209,63 @@ function createImage()
 	var url = DOMURL.createObjectURL(svg);
 
 	img.onload = function() 
+	{				
+		if(1==2)
+		{
+			ctx.drawImage(img, 0, 0);
+			
+			var strMime = "image/png";
+			var imgData = canvas.toDataURL(strMime);	
+			console.log(imgData);
+
+			openFileImage(imgData.replace(strMime, "image/octet-stream"), "screenshot.png");	
+
+			DOMURL.revokeObjectURL(imgData);
+		}
+		else
+		{
+			arrImg_2 = img;
+			assemblyImage();			
+		}
+	};
+	img.src = url;	
+	
+
+	for ( var i = 0; i < arr.length; i++ )
 	{
-		ctx.drawImage(img, 0, 0);
+		arr[i].remove();
+	}	
+}
+
+
+// собираем 2 изображение в одно
+function assemblyImage() 
+{	
+	if(arrImg_1 && arrImg_2)
+	{
+		console.log(arrImg_1);
+		console.log(arrImg_2);
 		
+		var svg = document.querySelector('#svgFrame');
+		var canvas = document.createElement("canvas");
+		canvas.width = svg.clientWidth;
+		canvas.height = svg.clientHeight;
+		var ctx = canvas.getContext("2d");
+
+		ctx.drawImage(arrImg_1, 0, 0);
+		ctx.drawImage(arrImg_2, 0, 0);
+
 		var strMime = "image/png";
 		var imgData = canvas.toDataURL(strMime);	
-		console.log(imgData);
 
 		openFileImage(imgData.replace(strMime, "image/octet-stream"), "screenshot.png");	
 
+		var DOMURL = self.URL || self.webkitURL || self;
 		DOMURL.revokeObjectURL(imgData);
-	};
-	img.src = url;	
 
+		arrImg_1 = null;
+		arrImg_2 = null;
+	}
 }
 
 
